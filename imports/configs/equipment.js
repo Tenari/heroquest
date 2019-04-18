c3a64a789111fe0e47a573a9b35f329223dfb402
Jonathan Zapata <jonweszap@gmail.com>
2019-04-18 00:15:58 +0000

    #1 added battle axe

---
+++ imports/configs/equipment.js
@@ -0,0 +1,11 @@
+export const EQUIPMENT = {
+  battle_axe: {
+    key: 'battle_axe',
+    name: "Battle axe",
+    cost: 400,
+    type: "weapon",
+    bonus: 1,
+    hands: 2,
+    restricted_caricatures: ['wizard'],
+  }
+}
